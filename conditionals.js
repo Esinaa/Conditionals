788140e885586a789d9d01903e628b3843a96df5
Enam Okailey Tetteh-Quaye <chloeonet@gmail.com>
2024-02-13 15:22:47 +0000

    js conditionals

---
+++ conditionals.js
@@ -0,0 +1,39 @@
+// //arrays containing names of collegues
+// let namesOfColleagues = ['Enam', 'Phoebe', 'Mariama', 'Michael', 'Eric', 'Jay', 'Issac', 'Fiifi', 'Andrews', 'Felix']
+// console.log(namesOfColleagues);
+
+// //for loop
+// for (x = 0; x < namesOfColleagues.length; x++) {
+//    // document.write(namesOfColleagues[x] + '<br>');
+//     console.log(namesOfColleagues);
+// 
+
+// //if else statement
+// let drink = 'Alvaro';
+
+// if (drink === 'Fanta') {
+//     console.log('make purchase');
+// } else if (drink === 'Red-wine') {
+//     console.log('make purchase');
+// }
+// else if (drink === 'Ceres') {
+//     console.log('make purchase');
+// }
+
+// else if (drink === 'Don-simon') {
+//     console.log('make purchase');
+// } else {
+//     console.log('no purchase');
+// }
+
+// //functions 
+
+
+
+function addSum(x, y, z) {
+    let total = x + y + z;
+    return total;
+    
+}
+
+console.log(addSum(30, 80, 70));
